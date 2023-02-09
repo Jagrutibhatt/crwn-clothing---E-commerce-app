ffa3eca5e1740016af7ef6dc9cb0d592707a7894
Jagruti Bhatt <jagrutirbhatt@gmail.com>
2023-02-09 23:38:53 +0530

    updating our directory page with directory component and category

---
+++ src/components/category-item/category-item.components.jsx
@@ -0,0 +1,20 @@
+import "./category-item.components.scss";
+
+const CategoryItem = ({ category }) => {
+  /* destructuring the array */
+  const { title, id, imageUrl } = category;
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
